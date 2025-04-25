e03fa5e68490da08fba8e8e516600938c44df418
TechClusterHQ <136372560+TechClusterHQ@users.noreply.github.com>
2025-04-25 21:57:15 +0200

    revert: use offtopic channel id

--- GreetingWatcher/greetingwatcher.py
+++ GreetingWatcher/greetingwatcher.py
@@ -34,7 +34,7 @@ class GreetingWatcher(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.id != 1264855954743230556:
+        if message.channel.id != 1218208566817587362:
             return
 
         if message.author.bot:
