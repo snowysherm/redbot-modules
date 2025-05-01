dec3db0c0c57e6a53b8c05922bcfb3e547f5d55f
TechClusterHQ <136372560+TechClusterHQ@users.noreply.github.com>
2025-05-01 13:52:47 +0200

    fix: dont count streak if greeting time is incorrect

--- GreetingWatcher/greetingwatcher.py
+++ GreetingWatcher/greetingwatcher.py
@@ -54,7 +54,7 @@ class GreetingWatcher(commands.Cog):
         
         # streak
 
-        if "gumo" in message.content.lower() and message.author.id not in GreetingWatcher.gumo_users:
+        if "gumo" in message.content.lower() and message.author.id not in GreetingWatcher.gumo_users and self.is_greeting_correct("gumo"):
             GreetingWatcher.gumo_streak += 1
             GreetingWatcher.gumo_users.append(message.author.id)
 
